ba4d1e9a5b7cefca9b66320f3fcee96b97ff4397
Cadentem <kai.zahn@yahoo.de>
2026-08-31 17:19:22 +0200

    why does this event need to be canceled

--- src/main/java/de/cadentem/additional_enchantments/client/fluid_vision/FluidVisionHandler.java
+++ src/main/java/de/cadentem/additional_enchantments/client/fluid_vision/FluidVisionHandler.java
@@ -2,12 +2,16 @@ package de.cadentem.additional_enchantments.client.fluid_vision;
 
 import de.cadentem.additional_enchantments.attachments.AEDataAttachments;
 import de.cadentem.additional_enchantments.attachments.FluidVisionData;
+import de.cadentem.additional_enchantments.enchantments.fluid_vision.FluidVision;
 import de.cadentem.additional_enchantments.mixin.client.LiquidBlockRendererMixin;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.player.Player;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderBlockScreenEffectEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.client.event.ViewportEvent;
 import net.neoforged.neoforge.common.NeoForgeMod;
@@ -46,31 +50,54 @@ public class FluidVisionHandler {
             return;
         }
 
-        float multiplier = player.getExistingData(AEDataAttachments.FLUID_VISION)
+        float percent = player.getExistingData(AEDataAttachments.FLUID_VISION)
                 .map(data -> data.get(fluid, player.registryAccess()).percentage())
                 .orElse(1f);
 
-        if (Float.compare(multiplier, 1) == 0) {
+        if (Float.compare(percent, 1) == 0) {
             return;
         }
 
         float nearPlane = event.getNearPlaneDistance();
 
         if (nearPlane < 0) {
-            nearPlane *= (1 + multiplier);
+            nearPlane *= (1 + percent);
         } else {
-            nearPlane *= multiplier;
+            nearPlane *= percent;
         }
 
         event.setNearPlaneDistance(nearPlane);
 
         float farPlane = event.getFarPlaneDistance();
-        float fluidVisibility = Minecraft.getInstance().options.renderDistance().get().floatValue() * multiplier;
+        // Minecraft seems to use 96 as some sort of max., see AtmosphericFogEnvironment (from 26.1.2)
+        float fluidVisibility = 96 * (1 - percent);
 
         if (farPlane > fluidVisibility) {
             return;
         }
 
         event.setFarPlaneDistance(fluidVisibility);
+        event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public static void renderFireOverlay(final RenderBlockScreenEffectEvent event) {
+        if (event.getOverlayType() != RenderBlockScreenEffectEvent.OverlayType.FIRE) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        if (!player.fireImmune() && !player.isCreative() && !player.hasEffect(MobEffects.FIRE_RESISTANCE)) {
+            return;
+        }
+
+        boolean hasVision = player.getExistingData(AEDataAttachments.FLUID_VISION)
+                .map(data -> data.get(player.getEyeInFluidType(), player.registryAccess()) != FluidVision.Mapped.NONE)
+                .orElse(false);
+
+        if (hasVision) {
+            event.setCanceled(true);
+        }
     }
 }
